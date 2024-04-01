c8321cd47a4b387f594371b08b0c35759d8ea175
Arjun Verma <kshatriya.arjun.developer@gmail.com>
2024-04-01 12:59:42 +0530

    ADD: Cached image loading

--- lib/src/ui/widget/image.dart
+++ lib/src/ui/widget/image.dart
@@ -1,9 +1,14 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:quickui/quickui.dart';
 
 class Image_ extends StatelessWidget {
   final double? height;
   final double? width;
+
+  // Image related
+  final String? imageUrl;
 
   // SVG related
   final String? localSvgAsset;
@@ -13,6 +18,7 @@ class Image_ extends StatelessWidget {
   const Image_({
     super.key,
     this.localSvgAsset,
+    this.imageUrl,
     this.height,
     this.width,
     this.svgColor,
@@ -24,11 +30,33 @@ class Image_ extends StatelessWidget {
     return SizedBox(
       height: height,
       width: width,
-      child: Align(
-        child: iconGradient != null
-            ? _maskWithGradient(buildSvgIcon(isMaskingGradient: true))
-            : buildSvgIcon(),
-      ),
+      child: _buildImage(),
+    );
+  }
+
+  Widget _buildImage() {
+    if (imageUrl != null) {
+      return _buildNetworkImage();
+    } else if (localSvgAsset != null) {
+      return _buildSvgImage();
+    } else {
+      return Container_();
+    }
+  }
+
+  Widget _buildNetworkImage() {
+    return CachedNetworkImage(
+      imageUrl: imageUrl!,
+      height: height,
+      width: width,
+    );
+  }
+
+  Widget _buildSvgImage() {
+    return Align(
+      child: iconGradient != null
+          ? _maskWithGradient(buildSvgIcon(isMaskingGradient: true))
+          : buildSvgIcon(),
     );
   }
 
